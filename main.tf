f2705055d6c3e68326d05c15bfe1bb7845b70261
aung ko htet <aungkohtet717@gmail.com>
2023-09-23 07:03:38 +0000

    #1 aw ec2 deploy

--- main.tf
+++ main.tf
@@ -11,11 +11,19 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+variable "instance_type" {
+  type = string
+}
+
+locals {
+  project_name ="aungkohtet"
+}
+
 resource "aws_instance" "my_server" {
   ami           = "ami-03a6eaae9938c858c"
-  instance_type = "t2.micro"
+  instance_type = "var.instance_type"
 
   tags = {
-    Name = "Myserver"
+    Name = "Myserver-${local.project_name}"
   }
 }
