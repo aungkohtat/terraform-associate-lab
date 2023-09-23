32e268839b2d481d21c6bfbf99a61c0d3f6b6080
aung ko htet <aungkohtet717@gmail.com>
2023-09-23 15:21:59 +0000

    #1 update

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
 terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "aungkohtet"
+
+  #  workspaces {
+  #    name = "getting-started"
+  #  }
+  #}
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "aungkohtet"
+
+    workspaces {
+      name = "getting-started"
+    }
+  }
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "aws" {
-  region  = "us-east-1"
-}
-
-variable "instance_type" {
-  type = string
-}
-
 locals {
-  project_name ="aungkohtet"
-}
-
-resource "aws_instance" "my_server" {
-  ami           = "ami-03a6eaae9938c858c"
-  instance_type = "var.instance_type"
-
-  tags = {
-    Name = "Myserver-${local.project_name}"
-  }
+  project_name = "aungkohtet"
 }
